65556fe77b07a792290f5477694c831d62b76075
AKraja777 <ashokkumar7hitter@gmail.com>
2023-11-03 12:11:13 +0530

    crud.php

--- includes/crud.php
+++ includes/crud.php
@@ -18,10 +18,10 @@ class Database
 
 
 
-  //  private $db_host = "localhost";  // Change as required
-  //  private $db_user = "u743445510_cc";  // Change as required
-  //  private $db_pass = "Color@2022";  // Change as required
-  //  private $db_name = "u743445510_cc"; 
+    private $db_host = "localhost";  // Change as required
+    private $db_user = "u743445510_nextgen";  // Change as required
+    private $db_pass = "Nextgen@2023";  // Change as required
+    private $db_name = "u743445510_nextgen"; 
 
     // private $db_host = "localhost";  // Change as required
     // private $db_user = "root";  // Change as required
