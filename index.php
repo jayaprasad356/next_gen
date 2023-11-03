645a747af91cbefe6b453b8a15a5adeb3246ad93
AKraja777 <ashokkumar7hitter@gmail.com>
2023-11-03 13:30:45 +0530

    home.php

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ session_start();
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="icon" type="image/ico" href="dist/img/favicon.jpeg">
-	<title>Admin Login -A1 Ads</title>
+	<link rel="icon" type="image/ico" href="dist/img/favicons.jpeg">
+	<title>Admin Login -Next gen</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
