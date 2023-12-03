93e7afa4aa43ab170349549c9f5b3e82e40e8bc3
jayaprasad356 <jayaprasad356@gmail.com>
2023-12-04 00:42:46 +0530

    main

--- api/add_main_balance.php
+++ api/add_main_balance.php
@@ -36,127 +36,12 @@ $num = $db->numRows($res);
 
 
 if ($num == 1) {
-    $basic_wallet = $res[0]['basic_wallet'];
-    $premium_wallet = $res[0]['premium_wallet'];
-    $current_refers = $res[0]['current_refers'];
     $today_orders = $res[0]['today_orders'];
-    $target_refers = $res[0]['target_refers'];
     $status = $res[0]['status'];
-    $plan = $res[0]['plan'];
-    $media_wallet = $res[0]['media_wallet']; 
-    $old_plan = $res[0]['old_plan']; 
     $hiring_earnings = $res[0]['hiring_earnings']; 
     $orders_earnings = $res[0]['orders_earnings'];
+    $average_orders = $res[0]['average_orders'];
 
-    if($wallet_type == 'basic_wallet'){
-        $min_basic_withdrawal = 30;
-        if($status == 0){
-            $min_basic_withdrawal = 60;
-
-        }
-        if($plan == 'A2'){
-            $min_basic_withdrawal = 20;
-
-        }
-        if($plan == 'A1' && $status == 1){
-            $sql = "SELECT * FROM monthly_target WHERE user_id = $user_id AND status = 0";
-            $db->sql($sql);
-            $res = $db->getResult();
-            $num = $db->numRows($res);
-            if ($num >= 1){
-                $response['success'] = false;
-                $response['message'] = "Complete Last Month Target";
-                print_r(json_encode($response));
-                return false;
-
-            }
-        }
-        if ($basic_wallet < $min_basic_withdrawal) {
-            $response['success'] = false;
-            $response['message'] = "Minimum ₹".$min_basic_withdrawal." to add balance";
-            print_r(json_encode($response));
-            return false;
-        }
-        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'basic_wallet','$datetime',$basic_wallet)";
-        $db->sql($sql);
-        $sql = "UPDATE users SET balance= balance + $basic_wallet,earn = earn + $basic_wallet,basic_wallet = basic_wallet - $basic_wallet WHERE id=" . $user_id;
-        $db->sql($sql);
-
-
-    }
-    if($wallet_type == 'premium_wallet'){
-        if ($plan == 'A1' && $old_plan == 0) {
-            $response['success'] = false;
-            $response['message'] = "Disabled";
-            print_r(json_encode($response));
-            return false;
-        }
-        if ($current_refers < $target_refers && $plan == 'A1') {
-            $response['success'] = false;
-            $response['message'] = "Minimum ".$target_refers." refers to add balance";
-            print_r(json_encode($response));
-            return false;
-        }
-        if ($premium_wallet < 120  && $plan == 'A1') {
-            $response['success'] = false;
-            $response['message'] = "Minimum ₹120 to add balance";
-            print_r(json_encode($response));
-            return false;
-        }
-        if($plan == 'A2'){
-            $premium_wallet = 700;
-            $sql_query = "SELECT * FROM `premium_refer_bonus` WHERE user_id = $user_id AND status = 0";
-            $db->sql($sql_query);
-            $res = $db->getResult();
-            $num = $db->numRows($res);
-
-            if($num>=1){
-                if ($premium_wallet < 700 ) {
-                    $response['success'] = false;
-                    $response['message'] = "Minimum ₹700 to add balance";
-                    print_r(json_encode($response));
-                    return false;
-                }
-                $premium_id = $res[0]['id'];
-                $sql = "UPDATE premium_refer_bonus SET status= 1 WHERE id=" . $premium_id;
-                $db->sql($sql);
-    
-            }else{
-                $response['success'] = false;
-                $response['message'] = "Refer 1 Person to get ₹700";
-                print_r(json_encode($response));
-                return false;
-
-            }
-
-        }
-
-        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'premium_wallet','$datetime',$premium_wallet)";
-        $db->sql($sql);
-        $sql = "UPDATE users SET balance= balance + $premium_wallet,earn = earn + $premium_wallet,premium_wallet = premium_wallet - $premium_wallet WHERE id=" . $user_id;
-        $db->sql($sql);
-    
-    }
-    if($wallet_type == 'media_wallet'){
-        $min_withdrawal = 2000;
-        if ($media_wallet < $min_withdrawal) {
-            $response['success'] = false;
-            $response['message'] = "Minimum ₹".$min_withdrawal." to add balance";
-            print_r(json_encode($response));
-            return false;
-        }
-
-        $response['success'] = false;
-        $response['message'] = "Disabled";
-        print_r(json_encode($response));
-        return false;
-        $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'media_wallet','$datetime',$media_wallet)";
-        $db->sql($sql);
-        $sql = "UPDATE users SET balance= balance + $media_wallet,earn = earn + $media_wallet,media_wallet = media_wallet - $media_wallet WHERE id=" . $user_id;
-        $db->sql($sql);
-
-
-    }
     if($wallet_type == 'hiring_earnings'){
         if ($hiring_earnings <= 0) {
             $response['success'] = false;
@@ -166,7 +51,7 @@ if ($num == 1) {
         }
         $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'hiring_earnings','$datetime',$hiring_earnings)";
         $db->sql($sql);
-        $sql = "UPDATE users SET hiring_earnings= hiring_earnings + $hiring_earnings,earn = earn + $hiring_earnings  WHERE id=" . $user_id;
+        $sql = "UPDATE users SET hiring_earnings= hiring_earnings - $hiring_earnings,earn = earn + $hiring_earnings,balance = balance + $hiring_earnings  WHERE id=" . $user_id;
         $db->sql($sql);
     }
     if($wallet_type == 'orders_earnings'){
@@ -176,9 +61,35 @@ if ($num == 1) {
             print_r(json_encode($response));
             return false;
         }
+        $today = date("N");
+        if($average_orders >= 400 && $average_orders < 500 && $today != 1){
+            $response['success'] = true;
+            $response['message'] = "Enable on Monday only";
+            print_r(json_encode($response));
+            return false;
+
+        }
+
+        if($average_orders >= 300 && $average_orders < 400 && $today != 1){
+            $response['success'] = true;
+            $response['message'] = "Enable on Monday only";
+            print_r(json_encode($response));
+            return false;
+
+        }
+
+        if($average_orders < 300 ){
+            $response['success'] = true;
+            $response['message'] = "Disabled";
+            print_r(json_encode($response));
+            return false;
+
+        }
+
+
         $sql = "INSERT INTO transactions (`user_id`,`type`,`datetime`,`amount`) VALUES ($user_id,'orders_earnings','$datetime',$orders_earnings)";
         $db->sql($sql);
-        $sql = "UPDATE users SET orders_earnings= orders_earnings + $orders_earnings,earn = earn + $orders_earnings  WHERE id=" . $user_id;
+        $sql = "UPDATE users SET orders_earnings= orders_earnings - $orders_earnings,earn = earn + $orders_earnings,balance = balance + $orders_earnings  WHERE id=" . $user_id;
         $db->sql($sql);
     }
 
