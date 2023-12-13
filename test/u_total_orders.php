2626bd9a9b7d5f12d41dd0a7958c767a6b0471b8
jayaprasad356 <jayaprasad356@gmail.com>
2023-12-14 00:58:14 +0530

    u_total

---
+++ test/u_total_orders.php
@@ -0,0 +1,41 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+
+
+$sql = "SELECT * FROM `u_total`";
+$db->sql($sql);
+$res= $db->getResult();
+$num = $db->numRows($res);
+if ($num >= 1){
+    foreach ($res as $row) {
+        $user_id = $row['user_id'];
+        $total_orders = $row['total_orders'];
+
+        $sql = "UPDATE users SET total_orders = $total_orders WHERE id = $user_id";
+        $db->sql($sql);
+    
+
+    }
+    $response['success'] = true;
+    $response['message'] = "updated Successfully";
+    print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Users Not found";
+    print_r(json_encode($response));
+
+}
+?>
