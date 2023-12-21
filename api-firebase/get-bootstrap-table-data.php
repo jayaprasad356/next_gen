c4ed9ebf6a8c31004ee1e3a3fa2e79427df0267b
AKraja777 <ashokkumar7hitter@gmail.com>
2023-12-21 15:42:57 +0530

    header.php

--- api-firebase/get-bootstrap-table-data.php
+++ api-firebase/get-bootstrap-table-data.php
@@ -1051,10 +1051,11 @@ if (isset($_GET['table']) && $_GET['table'] == 'staff_withdrawals') {
     if (isset($_GET['order']))
         $order = $db->escapeString($fn->xss_clean($_GET['order']));
 
-    if (isset($_GET['search']) && !empty($_GET['search'])) {
-        $search = $db->escapeString($fn->xss_clean($_GET['search']));
-        $where .= "AND s.mobile like '%" . $search . "%'";
-    }
+        if (isset($_GET['search']) && !empty($_GET['search'])) {
+            $search = $db->escapeString($fn->xss_clean($_GET['search']));
+            $where .= "AND s.name like '%" . $search . "%' OR t.amount like '%" . $search . "%' OR t.id like '%" . $search . "%'  OR t.type like '%" . $search . "%' OR s.mobile like '%" . $search . "%' ";
+        }
+        
     if (isset($_GET['sort'])) {
         $sort = $db->escapeString($_GET['sort']);
     }
