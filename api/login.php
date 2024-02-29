2be88851ef2673b768c798a84230ec79c874ac8b
jayaprasad356 <jayaprasad356@gmail.com>
2024-02-29 09:38:13 +0530

    login

--- api/login.php
+++ api/login.php
@@ -56,7 +56,7 @@ if ($num == 1){
         $num = $db->numRows($res);
         $login_time = $res[0]['login_time'];
 
-        if (!empty($res[0]['login_time']) || $login_time != '0000-00-00 00:00:00') {
+        if ($login_time != '0000-00-00 00:00:00') {
             $sql_update_blocked = "UPDATE users SET blocked = 1 WHERE mobile ='$mobile'";
             $db->sql($sql_update_blocked);
             
